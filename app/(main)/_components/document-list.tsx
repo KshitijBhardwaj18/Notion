736b4178ea6c7fbf47381bb42230f8ad4d90a884
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-11-01 23:12:58 +0530

    added comment

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,64 +1,82 @@
-"use client"
+"use client";
 
-import { Doc, Id } from "@/convex/_generated/dataModel"
+// जय श्री राम 
 
-import { useParams, useRouter } from "next/navigation"
+import { Doc, Id } from "@/convex/_generated/dataModel";
 
-import { useState } from "react"
+import { useParams, useRouter } from "next/navigation";
 
-import {useQuery} from "convex/react";
+import { useState } from "react";
+
+import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
 
 import { Item } from "./item";
 
-interface DocumentListProps{
-    parentDocumentId?: Id<"documents">
-    level?: number;
-    data?: Doc<"documents">[];
+import { cn } from "@/lib/utils";
 
+interface DocumentListProps {
+  parentDocumentId?: Id<"documents">;
+  level?: number;
+  data?: Doc<"documents">[];
 }
 
 export const DocumentList = ({
-    parentDocumentId,
-    level = 0
-} : DocumentListProps) => {
+  parentDocumentId,
+  level = 0,
+}: DocumentListProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
 
-    const params = useParams();
-    const router = useRouter();
-    const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+  const onExpand = (documentId: string) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId],
+    }));
+  };
 
-    const onExpand = (documentId: string) => {
-        setExpanded(prevExpanded => ({
-            ...prevExpanded,
-            [documentId]: !prevExpanded[documentId]
-        }))
-    };
+  const documents = useQuery(api.documents.getSidebar, {
+    parentDocument: parentDocumentId,
+  });
 
-    const documents = useQuery(api.documents.getSidebar, {parentDocument : parentDocumentId});
+  const onRedirect = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
 
-    const onRedirect = (documentId: string ) => {
-        router.push(`/documents/${documentId}`);
-    };
+  if (documents === undefined) {
+    return (
+      <>
+        <Item.Skeleton level={level} />
+        {level === 0 && (
+          <>
+            <Item.Skeleton level={level} />
+            <Item.Skeleton level={level} />
+          </>
+        )}
+      </>
+    );
+  }
 
-    if (documents === undefined){
-        return(
-            <>
-            <Item.Skeleton level={level}/>
-            {level === 0 && (
-                <>
+  return (
+    <>
+      <p
+        style={{ paddingLeft: level ? `${level * 12 + 25}px` : undefined }}
+        className={cn(
+          "hidden text-sm font-medium text-muted-foreground/80 ",
+          expanded && "last : block",
+          level === 0 && "hidden"
+        )}
+      >
+        No pages inside
+      </p>
+      {documents.map((document) => (
+        <div key={document._id}>
+            <Item id={}/>
+        </div>
 
-
-
-                </>
-            )}
-            </>
-
-        )
-
-    }
-
-
-
-    
-}
+      ))}
+    </>
+  );
+};
